f2c4aeeaf3b23e149514f9159994ac740f2e176c
Adam Perinay <perinay.adam@gmail.com>
2020-04-29 15:26:59 +0200

    Okno

--- Program.py
+++ Program.py
@@ -1,6 +1,16 @@
+import tkinter
+
+
 class Program:
     def __init__(self):
-        pass
+        # Vytvorenie okna
+        tk = tkinter.Tk()
+        tk.geometry("800x600")
+        tk.resizable(0, 0)
+        tk.title("UNO")
+
+        # zobrazenie okna
+        tk.mainloop()
 
 
 Program()
